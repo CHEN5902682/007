fb1481de4642c17663d4bb05acf127435133340e
Lemon <Lemon@LAPTOP-OS9BCBCC>
2019-12-27 16:33:17 +0800

    初次上传项目

---
+++ src/main/java/edu/swjtuhc/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package edu.swjtuhc.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.swjtuhc.demo.model.SysUser;
+import edu.swjtuhc.demo.service.UserService;
+import net.sf.json.JSONObject;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	UserService userService;
+	
+	
+	@RequestMapping("/register")
+	public JSONObject register(@RequestBody SysUser user) {
+		JSONObject result = new JSONObject();
+		
+//		调用Service
+		int i = userService.register(user);
+		result.put("state", i);
+		
+		
+		return result;
+	}
+}
